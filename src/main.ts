66e3c5f72d089606587d008e6a2bf69df1a8716f
MorSof <morsof48@gmail.com>
2023-05-19 11:19:59 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Bars Service')
+    .setDescription('Bars Service API description')
+    .setVersion('1.0')
+    .addTag('Bars Service')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3005);
 }
 bootstrap();
